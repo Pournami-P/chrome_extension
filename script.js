8e10b875f22083e6ef5acfc04510cc6696bd419f
Pournami <pournamip2005@gmail.com>
2026-06-09 11:25:37 +0530

    Final chrome extension

--- script.js
+++ script.js
@@ -13,10 +13,6 @@ if(leadsFromLocalStorage){
     render(myLeads)
 }
 
-const tabs = [
-    {url: "https://www.linkedin.com/in/per-harald-borgen/"}
-]
-
 tabBtn.addEventListener("click",function(){
     chrome.tabs.query({active:true, currentWindow:true},function(tabs){
         myLeads.push(tabs[0].url)
