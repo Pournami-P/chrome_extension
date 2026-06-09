6118e459e259b6ce330a6d6e1da1891e34b889b8
Pournami <pournamip2005@gmail.com>
2026-06-09 11:24:35 +0530

    Final chrome extension

--- script.js
+++ script.js
@@ -18,7 +18,11 @@ const tabs = [
 ]
 
 tabBtn.addEventListener("click",function(){
-    
+    chrome.tabs.query({active:true, currentWindow:true},function(tabs){
+        myLeads.push(tabs[0].url)
+        localStorage.setItem("myLeads", JSON.stringify( myLeads ))
+        render(myLeads)
+    })
 })
 
 deleteBtn.addEventListener("dblclick",function(){
