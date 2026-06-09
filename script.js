035c290bbd790e37452ca83d269c41d57794322f
Pournami <pournamip2005@gmail.com>
2026-06-09 10:18:41 +0530

    New updations

--- script.js
+++ script.js
@@ -1,12 +1,38 @@
-const myLeads=[]
+let myLeads=[]
 const inputlEl=document.getElementById("input-el")
 const intputBtn=document.getElementById("input-btn")
 const ulEl=document.getElementById("ul-el")
+const deleteBtn=document.getElementById("delete-btn")
+const tabBtn=document.getElementById("tab-btn")
+
+const leadsFromLocalStorage=JSON.parse(localStorage.getItem("myLeads"))
+console.log(leadsFromLocalStorage)
+
+if(leadsFromLocalStorage){
+    myLeads=leadsFromLocalStorage
+    render(myLeads)
+}
+
+const tabs = [
+    {url: "https://www.linkedin.com/in/per-harald-borgen/"}
+]
+
+tabBtn.addEventListener("click",function(){
+    
+})
+
+deleteBtn.addEventListener("dblclick",function(){
+    localStorage.clear();
+    myLeads=[]
+    render(myLeads)
+})
+
 
 intputBtn.addEventListener("click",function(){
     myLeads.push(inputlEl.value)
-    inputlEl.value=" "
-    renderLeads()
+    inputlEl.value=""
+    localStorage.setItem("myLeads",JSON.stringify(myLeads))
+    render(myLeads)
 })
 
 // function renderLeads(){
@@ -16,10 +42,17 @@ intputBtn.addEventListener("click",function(){
 //     }
 // }
 
-function renderLeads(){
-    listItems=" "
-    for(let i=0;i<myLeads.length;i++){
-        listItems+="<li>"+myLeads[i]+"</li>"
+function render(leads){
+    let listItems=" "
+    for(let i=0;i<leads.length;i++){
+        //listItems+="<li><a target='_blank' href='"+myLeads[i]+"'>"+myLeads[i]+"</a></li>"
+        listItems+=`
+            <li>
+                <a target='_blank' href='${leads[i]}'>
+                    ${leads[i]}
+                </a>
+            </li>        
+        `
     }
     ulEl.innerHTML=listItems
 }
